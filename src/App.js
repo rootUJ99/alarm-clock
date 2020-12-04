e571583cda426d8df9492a67f99d9c92585e3227
Ujwal <ujwal99arak@gmail.com>
2020-12-05 04:02:36 +0530

    added some stuff for alarm

--- src/App.js
+++ src/App.js
@@ -1,61 +1,84 @@
 import {useEffect, useState} from 'react';
 import './App.css';
 
-const alramTicksArr = [...new Array(60)].map((it, index) => index + 1);
+const alramTicksArr = [...new Array(60)].map((it, index) => index );
+const hourArr = [...new Array(24)].map((it, index) => index+1 );
+
 
 const  App = () => {
   const [currDateTime, setCurrDateTime] = useState(new Date());
-  const [alramDateTime, setAlramDateTime] = useState();
+  const [alramDateTime, setAlramDateTime] = useState(null);
+  const [clockForm, setClockForm] = useState({ hour: 1, minute: 0, second: 0 });
 
   useEffect(() => {
     const TimerId = setInterval(() => {
       setCurrDateTime(new Date());
-      if (alramDateTime && currDateTime === alramDateTime) {
-        console.log('tick tick');
-      }
     }, 1000);
+    if (alramDateTime && (currDateTime.toLocaleTimeString() === alramDateTime.toLocaleTimeString())) {
+        console.log('tick tick');
+        console.log(currDateTime.getTime(), alramDateTime.getTime());
 
+      }
     return () => {
       clearInterval(TimerId)
     }
-  },[currDateTime, alramDateTime]);
+  },[alramDateTime, currDateTime]);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(clockForm);
+    const date = new Date();
+    date.setHours(Number(clockForm.hour))
+    date.setMinutes(Number(clockForm.minute))
+    date.setSeconds(Number(clockForm.second))
+
+    setAlramDateTime(
+          new Date(date)
+    );
+
+  }
+
+  const handleChange = (e) => {
+    setClockForm({
+      ...clockForm,
+      [e.target.name]:e.target.value
+    });
+  }
 
   return (
     <div className="App">
       {currDateTime.toLocaleTimeString()}
+    <form onSubmit={handleSubmit}>
+      <select value={clockForm.hour} name='hour' onChange={handleChange}>
+        {hourArr.map(it=> (<option 
+          key={it} 
+          value={it}> 
+             {it}
+          </option>)
+        )}
+      </select>
+      <select value={clockForm.minute} name='minute' onChange={handleChange}>
+        {alramTicksArr.map(it=> (<option 
+          key={it} 
+          value={it}> 
+             {it}
+          </option>)
+        )}
+      </select>
+      <select value={clockForm.second} name='second' onChange={handleChange}>
+        {alramTicksArr.map(it=> (<option 
+          key={it} 
+          value={it}> 
+             {it}
+          </option>)
+        )}
+      </select>
+        <button type="submit"> 
+        SetAlram
+      </button>
+    </form>
       {alramDateTime && alramDateTime.toLocaleTimeString()}
-    <select>
-      {alramTicksArr.map(it=> (<option 
-        key={it} 
-        value={it}> 
-           {it}
-        </option>)
-      )}
-    </select>
-    <select>
-      {alramTicksArr.map(it=> (<option 
-        key={it} 
-        value={it}> 
-           {it}
-        </option>)
-      )}
-    </select>
-    <select>
-      {alramTicksArr.map(it=> (<option 
-        key={it} 
-        value={it}> 
-           {it}
-        </option>)
-      )}
-    </select>
-      <button onClick={()=>{
-        const date = new Date();
-        setAlramDateTime(
-          new Date(date.setMinutes(date.getMinutes() + 2))
-        );
-      }}>
-      SetAlram
-    </button>
     </div>
   );
 }
